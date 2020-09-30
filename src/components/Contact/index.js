4750c292a7a38b0cd38eb372191c7a5d7eab5c4c
J Mulder <jlmulder2014@gmail.com>
2020-09-30 16:14:28 -0700

    dynamic contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,18 +1,43 @@
 //to use hook
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
   //hook
   // clear input field on the component loading
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const { name, email, message } = formState;
+  //initial state is an empty string
+  const [errorMessage, setErrorMessage] = useState('');
 
   function handleChange(e) {
-    //... spread operator to retain the other key-value pairs in the object (otherwise would only contain name:value)
-    //uses setFormState function to update formState value for the name property
-    //.name refers to name attribute not name key
-    //use [] to create dynamic property names
-    setFormState({ ...formState, [e.target.name]: e.target.value })
+    //target email <input> element
+    if (e.target.name === 'email') {
+      //store return value in "isValid"
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      //check "message" and name "form" values
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    //only update if valid
+    if (!errorMessage) {
+      //... spread operator to retain the other key-value pairs in the object (otherwise would only contain name:value)
+      //uses setFormState function to update formState value for the name property
+      //.name refers to name attribute not name key
+      //use [] to create dynamic property names
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   }
   // console.log(formState);
 
@@ -20,29 +45,35 @@ function ContactForm() {
     e.preventDefault();
     console.log(formState);
   }
-  
+
   // JSX
   //htmlFor = for 
   //defaultValue={formState.name} --- assign initial state values to the input fields in the DOM
   //default value pulling from formState
   //on Change (ex. keystroke in field), fire handleChange function
+  // && if true, will render second part
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
-        <button type="submit">Submit</button>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit" data-testid="button">Submit</button>
       </form>
     </section>
   )
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,10 +3,13 @@ import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
+  //deconstruct functions from props
   const {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
   useEffect(() => {
@@ -25,21 +28,22 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li>
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li className={`mx-1 ${
               //to change color when category is selected
-              currentCategory.name === category.name && 'navActive'
+              currentCategory.name === category.name && !contactSelected && 'navActive'
               }`} key={category.name}>
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
