7c3be35084dc8bdd5cb5ab80c918494194a06a32
J Mulder <jlmulder2014@gmail.com>
2020-09-30 17:07:49 -0700

    modal creation

---
+++ src/components/Modal/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//destructure props from the PhotoList component into currentPhoto in the Modal parameter
+function Modal({currentPhoto}) {
+  //destructure currentPhoto properties into constants to assign their values into the modal
+  const {name, category, description, index} = currentPhoto;
+  return (
+    <div className="modalBackdrop">
+      <div className="modalContainer">
+        <h3 className="modalTitle">{name}</h3>
+        <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+        <p>{description}</p>
+        <button type="button">
+          Close this modal
+    </button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
